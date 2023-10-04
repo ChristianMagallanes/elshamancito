1752a1356159fabeedea493fa4f1a533dd2f4f22
christian magallanes <dar4del-x@hotmail.com>
2023-10-04 07:45:49 -0300

    primer commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function design() {
+  let seccion = document.getElementById("donacioness");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
+function about() {
+  let seccion = document.getElementById("sobremi");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
+function inicio() {
+  let seccion = document.getElementById("header");
+  seccion.scrollIntoView({ behavior: "smooth" })
+}
+
+
