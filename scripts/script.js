84c111a27c1aa3f9a0585ef13ec0c153f4bdd1ec
christian magallanes <dar4del-x@hotmail.com>
2023-10-04 20:40:49 -0300

    listo

--- scripts/script.js
+++ scripts/script.js
@@ -14,12 +14,5 @@ function social() {
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const navbar = document.querySelector('.navbar');
 
-  hamburger.addEventListener('click', function () {
-    navbar.classList.toggle('active');
-  });
-});
 
